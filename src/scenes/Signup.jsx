3ea21336110ec879d03fdd61da4be3aa601c6db3
GiannaDavila <giannagiannadavila@gmail.com>
2022-11-08 11:48:34 -0500

    first commit

--- src/scenes/Signup.jsx
+++ src/scenes/Signup.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import { initializeApp } from "firebase/app"
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider,
-     signInWithPopup } from "firebase/auth"
+import { getAuth,createUserWithEmailAndPassword  } from "firebase/auth"
 
 const firebaseConfig = {
     apiKey: "AIzaSyDA01Uy5Pzk9yAhWoM84lgnTj4ZpN085rQ",
@@ -12,7 +11,7 @@ const firebaseConfig = {
     appId: "1:14363149803:web:6f599e0658cb25d2af05ae"
 };
 
-export default function Login({ setUser }) {
+export default function Signup({ setUser }) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const handleLogin = async (e) => {
@@ -20,23 +19,13 @@ export default function Login({ setUser }) {
         //prevent from webcite refreshing
         const app = initializeApp(firebaseConfig)//connects to fiebase 
         const auth = getAuth(app)// conects us to firebase auth 
-        const response = await signInWithEmailAndPassword(auth, email, password)// signs us in 
+        const response = await createUserWithEmailAndPassword(auth, email, password)// signs us in 
             .catch(alert);
-        setUser(response.user)
-    }
-    const handleGoogleLogin = async (e) => {
-        e.preventDefault()
-        //prevent from webcite refreshing
-        const app = initializeApp(firebaseConfig)//connects to fiebase 
-        const auth = getAuth(app)// conects us to firebase auth 
-        const provider = new GoogleAuthProvider()
-        const response = await signInWithPopup(auth, provider)
-        .catch(alert);
         setUser(response.user)
     }
     return (
         <>
-            <h1>Login</h1>
+            <h1>Signup</h1>
             <form onSubmit={handleLogin}>
                 <label htmlFor="email">Email:
                     <input type="email"
@@ -52,11 +41,9 @@ export default function Login({ setUser }) {
                         onChange={e => setPassword(e.target.value)}
                         placeholder="*********" />
                     <br />
-                    <button type="submit">Login</button>
+                    <button type="submit">Signup</button>
                 </label>
             </form>
-            <br/>
-            <button onClick={handleGoogleLogin}>Sign in with google</button>
         </>
     )
 }
